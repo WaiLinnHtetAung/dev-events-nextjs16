9193f9836786f38594e3b658bd880aa5e22e9dcd
WaiLinnHtetAung <wailinhtetaung007@gmail.com>
2026-02-08 22:42:55 +0630

    setup cloudinary for image upload

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import { Event } from "@/database";
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
     try{
@@ -15,12 +16,41 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ message: "Invalid JSON data format"}, { status: 400 });
         }
 
+        const file = formData.get("image") as File;
+
+        if(!file) return NextResponse.json({ message: "Image file is required"}, { status: 400 });
+
+        // Upload image to Cloudinary
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: "image", folder: "DevEvent" }, (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            }).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
+            
+
         const createdEvent = await Event.create(event);
 
         return NextResponse.json({ message: "Event created successfully", event: createdEvent }, { status: 201 });
     } catch (e) {
         console.log(e);
         return NextResponse.json({ message: "Event creation failed", error: e instanceof Error ? e.message : "Unknown error" }, { status: 500 });
+    }
+}
+
+export async function GET() {
+    try {
+        await connectDB();
+        const events = await Event.find().sort({ createdAt: -1 });
+        return NextResponse.json({ message: "Events fetched successfully", events }, { status: 200 });
+    } catch(e) {
+        console.log(e);
+        return NextResponse.json({ message: "Failed to fetch events", error: e instanceof Error ? e.message : "Unknown error" }, { status: 500 });
     }
 }
 
